fa6bab2b341571483ba87f4bfb8d7d201ef2aec6
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-21 22:49:55 +0700

    new(factory): create AddressFactory

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -2,12 +2,9 @@
 
 namespace Database\Factories;
 
-
 use App\Models\Address;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
-
 
 class AddressFactory extends Factory
 {
@@ -16,15 +13,11 @@ class AddressFactory extends Factory
 	public function definition(): array
 	{
 		return [
-				'address_line_1' => $this->faker->address(),
-				'address_line_2' => $this->faker->address(),
-				'city' => $this->faker->city(),
-				'is_primary' => $this->faker->boolean(),
+				'address_line_1' => fake('vi_VN')->streetAddress(),
+				'city' => fake('vi_VN')->city(),
+				'is_primary' => true,
 				'created_at' => Carbon::now(),
 				'updated_at' => Carbon::now(),
-				'deleted_at' => Carbon::now(),
-				
-				'user_id' => User::factory(),
 		];
 	}
 }
