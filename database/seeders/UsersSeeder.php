5679a17754943c4b832ade0e34898ff41975acda
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-21 22:51:09 +0700

    new(seed): create UsersSeeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -2,14 +2,17 @@
 
 namespace Database\Seeders;
 
-
+use App\Models\User;
+use App\Models\Address;
 use Illuminate\Database\Seeder;
-
 
 class UsersSeeder extends Seeder
 {
-	public function run(): void
-	{
-		
-	}
+    public function run(): void
+    {
+        User::factory()
+            ->count(30)
+            ->has(Address::factory()->count(1))
+            ->create();
+    }
 }
