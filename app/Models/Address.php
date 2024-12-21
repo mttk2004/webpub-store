3f80db63f8985331a0634e7f327932bc2d9f1a35
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-21 22:55:14 +0700

    new(model): create Address model

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -11,34 +11,20 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
-	use HasUuids, HasFactory;
-	
-	
-	public $timestamps = false;
-	protected $fillable
-			= [
-					'user_id',
-					'address_line_1',
-					'address_line_2',
-					'city',
-					'is_primary',
-					'created_at',
-					'updated_at',
-					'deleted_at',
-			];
-	
-	public function user(): BelongsTo
-	{
-		return $this->belongsTo(User::class);
-	}
-	
-	protected function casts(): array
-	{
-		return [
-				'is_primary' => 'boolean',
-				'created_at' => 'datetime',
-				'updated_at' => 'datetime',
-				'deleted_at' => 'datetime',
-		];
-	}
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'is_primary' => 'boolean',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'deleted_at' => 'datetime',
+        ];
+    }
 }
