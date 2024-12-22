52aa30c9abbd304317eb3391eae996f1eec4e48e
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-22 17:19:24 +0700

    update(routes): add new routes for Auth and Guest

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Guest\BookController;
+use App\Http\Controllers\Guest\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome')->name('home');
+
+Route::group(['middleware' => 'guest'], function () {
+	// Hiển thị sách và thể loại
+	Route::resource('books', BookController::class)->only(['index', 'show']);
+	Route::resource('categories', CategoryController::class)->only(['index', 'show']);
+	
+	// Đăng ký
+	Route::get('/register', [RegisterController::class, 'index'])->name('register');
+	Route::post('/register', [RegisterController::class, 'register']);
+	
+	// Đăng nhập
+	Route::get('/login', [LoginController::class, 'index'])->name('login');
+	Route::post('/login', [LoginController::class, 'login']);
 });
