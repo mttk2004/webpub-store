f50d4e88a9edf99bd3a688e029f028483e7cddc5
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-24 10:10:36 +0700

    update(model): model User extends Authenticatable to Auth functionality

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,48 +2,46 @@
 
 namespace App\Models;
 
-
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-
-class User extends Model
+class User extends Authenticatable implements AuthenticatableContract
 {
-	use HasUuids, HasFactory, SoftDeletes;
-	
-	
-	protected $fillable
-			= [
-					'role',
-					'first_name',
-					'last_name',
-					'email',
-					'password',
-					'avatar',
-					'email_verified_at',
-					'remember_token',
-			];
-	protected $hidden
-			= [
-					'password',
-					'remember_token',
-			];
-	
-	public function casts(): array
-	{
-		return [
-				'email_verified_at' => 'datetime',
-				'created_at' => 'datetime',
-				'updated_at' => 'datetime',
-				'deleted_at' => 'datetime',
-		];
-	}
-	
-	public function addresses(): HasMany
-	{
-		return $this->hasMany(Address::class);
-	}
+    use HasUuids, HasFactory, SoftDeletes;
+
+    protected $fillable
+    = [
+        'role',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'avatar',
+        'email_verified_at',
+        'remember_token',
+    ];
+    protected $hidden
+    = [
+        'password',
+        'remember_token',
+    ];
+
+    public function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'deleted_at' => 'datetime',
+        ];
+    }
+
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(Address::class);
+    }
 }
