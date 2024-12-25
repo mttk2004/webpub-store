bbe99de01f17cc0b372ea6b139285923ee6d83a0
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-25 10:51:19 +0700

    update(ui): make ui more beautiful and interactive

---
+++ resources/js/flyout-menu.js
@@ -0,0 +1,9 @@
+/**
+ *  Project: webpub-store
+ *  File: flyout-menu.js
+ *  Created: 10:58 AM, 12/24/24
+ *  Author: Mai Tran Tuan Kiet
+ *  "Family is where life begins and love never ends."
+ */
+
+
---
+++ resources/js/modal-search.js
@@ -0,0 +1,9 @@
+/**
+ *  Project: webpub-store
+ *  File: modal-search.js
+ *  Created: 8:14 PM, 12/24/24
+ *  Author: Mai Tran Tuan Kiet
+ *  "Family is where life begins and love never ends."
+ */
+
+
