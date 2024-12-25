9c00019c6b72634ee5f41be161e1277dab9862a6
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-25 10:56:47 +0700

    update(view): make homepage with a layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body>
-    <h1 class='text-red-500 font-black text-2xl'>Hello World</h1>
-    <p>Con mèo mà trèo cây cao</p>
-    @auth
-        <p>Hello, {{ auth()->user()->email }}</p>
-    @else
-        <p>Hello, guest</p>
-    @endauth
-</body>
-
-</html>
+<x-layout>
+    <!-- Your content here -->
+    <p>sjfhjdhghjfdgkhjdfghkj</p>
+</x-layout>
