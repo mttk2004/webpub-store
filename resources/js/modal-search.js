1f45e6c20b686c7299d755c6dacc080c2a93c817
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-25 10:58:16 +0700

    update(js): make modal search using jquery

--- resources/js/modal-search.js
+++ resources/js/modal-search.js
@@ -7,3 +7,68 @@
  */
 
 
+import $ from 'jquery';
+
+
+$(document).ready(function () {
+	const searchModal = $('.modal-search');
+	const inputToggle = $('#modal-search-open');
+	const closeButton = $('#modal-search-close');
+	const modalBg = $('#modal-bg');
+	const modalPanel = $('#modal-panel');
+	
+	const open = () => {
+		searchModal.toggleClass('invisible');
+		modalBg.removeClass('opacity-0').addClass('opacity-100 backdrop-blur-[6px]');
+		modalPanel.removeClass('translate-y-8 scale-75 opacity-0')
+							.addClass('translate-y-0 scale-100 opacity-100');
+		inputToggle.blur();
+	}
+	
+	const close = () => {
+		modalPanel.removeClass('translate-y-0 scale-100 opacity-100')
+							.addClass('translate-y-4 scale-75 opacity-0');
+		modalBg.removeClass('opacity-100 backdrop-blur-[6px]').addClass('opacity-0');
+		searchModal.toggleClass('invisible');
+		inputToggle.blur();
+	}
+	
+	// open modal and unfocus the input
+	inputToggle.off('click input').on('click input', function () {
+		open();
+		
+		modalBg.off('click').on('click', function (event) {
+			event.stopPropagation();
+			if ($(event.target).closest('#modal-panel').length) return;
+			
+			close();
+		});
+	});
+	
+	// if user typing on the input, remove the value and unfocus the input
+	inputToggle.on('input', function () {
+		$(this).val('');
+		$(this).blur();
+	});
+	
+	// or use Ctrl + K to toggle the search modal
+	$(document).on('keydown', function (event) {
+		if (event.altKey && event.key === 'k') {
+			searchModal.hasClass('invisible') ? open() : close();
+			
+			modalBg.off('click').on('click', function (event) {
+				event.stopPropagation();
+				if ($(event.target).closest('#modal-panel').length) return;
+				
+				close();
+			});
+		}
+	});
+	
+	// close modal and unfocus the input
+	closeButton.off('click').on('click', function (event) {
+		event.stopPropagation();
+		close()
+	});
+	
+});
