a9e31505460adac34ec067551ffee836da9fce28
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2024-12-31 12:59:57 +0700

    new(controllers): page controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\Book;
+
+
 class PageController extends Controller
 {
-	public function index()
+	public function home()
 	{
-		
+		$latestBooks = Book::latest()->take(10)->get();
+		return view('home', compact('latestBooks'));
 	}
 }
