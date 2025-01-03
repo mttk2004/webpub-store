634d4a6c48545735711b1aac9f5957b78554d0cf
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-03 15:04:22 +0700

    new(config): snowflake

---
+++ config/snowflake.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+		'start_time' => '2023-01-01', // Thời điểm bắt đầu (epoch)
+		'machine_id' => 1,            // ID máy (0-1023)
+		'sequence' => 0,              // Bộ đếm (0-4095)
+];
