8969cf19819d9b1e66ec2e6a0157ba9aba9cd79d
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-03 19:36:35 +0700

    update(model): new helper functions [parent, children] and local scope [languages, technologies]

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 
 class Category extends Model
@@ -19,4 +21,30 @@ class Category extends Model
 					'name',
 					'description',
 			];
+	
+	public function parent(): BelongsTo
+	{
+		return $this->belongsTo(Category::class, 'parent_id');
+	}
+	
+	public function children(): HasMany
+	{
+		return $this->hasMany(Category::class, 'parent_id');
+	}
+	
+	public function scopeLanguages($query)
+	{
+		// where parent's name is 'Ngôn ngữ'
+		return $query->whereHas('parent', function ($query) {
+			$query->where('name', 'Ngôn ngữ');
+		});
+	}
+	
+	public function scopeTechnologies($query)
+	{
+		// where parent's name is 'Công nghệ'
+		return $query->whereHas('parent', function ($query) {
+			$query->where('name', 'Công nghệ');
+		});
+	}
 }
