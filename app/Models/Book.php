67640ed4b26b7fdadbcf8f00aeba260791da274b
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-01-03 19:33:50 +0700

    update(model): new relationship for Book: categories

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -58,4 +58,9 @@ class Book extends Model
 	{
 		return $this->belongsToMany(Author::class);
 	}
+	
+	public function categories(): BelongsToMany
+	{
+		return $this->belongsToMany(Category::class, 'book_category');
+	}
 }
---
+++ database/seeders/BookCategorySeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Seeder;
+
+
+class BookCategorySeeder extends Seeder
+{
+	public function run(): void
+	{
+		
+	}
+}
